686365892a7c8b078c4bd769dee735c174880bf1
Cal <60569006+cbullard-dev@users.noreply.github.com>
2025-10-11 20:17:13 +0900

    Completed the copy and deletion functions. Added checking that will confirm that the new file exists in the new location before removing the old file from the original locaiton. Added developer flag that can be turned on so that testing can occur in the TESTING directory of the downloads directory. Tested on TESTING Tested on production
    Added unsorted files currently in downloads

--- SortDownloads.py
+++ SortDownloads.py
@@ -1,38 +1,65 @@
 import os
 from os import path, getenv, listdir
+import shutil
 
 SUCCESS = 0
 ERROR_GENERAL = 1
 ERROR_DIRECTORY_NOT_FOUND = 2
 
+DEVELOPER_BUILD = False
+
 documentTypes = ["pdf", "doc", "docx", "pages", "numbers"]
+flatDocumentTypes = ["txt", "csv", "json", "md"]
 softwareTypes = ["dmg", "pgk"]
-imageTypes = ["CR2", "png", "jpg", "jpeg"]
+imageTypes = ["cr2", "png", "jpg", "jpeg"]
+videoTypes = ["mp4", "mpg", "mpeg"]
 diskImageTypes = ["iso"]
 subtitleType = ["srt"]
 archiveType = ["zip", "tar", "gz"]
+windowsTypes = ["exe", "msi"]
 
 directoriesDict = {
     "Documents": documentTypes,
+    "Text_Files": flatDocumentTypes,
     "Software": softwareTypes,
     "Disk_Images": diskImageTypes,
     "Images": imageTypes,
+    "Videos": videoTypes,
     "Subtitles": subtitleType,
     "Archives": archiveType,
+    "Windows_Junk": windowsTypes,
 }
 
 homeDirectory = getenv("HOME")
 
-downloadPath = f"{homeDirectory}/Downloads/TESTING"
+downloadPath = f"{homeDirectory}/Downloads"
+
+if DEVELOPER_BUILD:
+    downloadPath = path.join(downloadPath, "TESTING")
 
 
 def main():
+
+    # Check if the directories exist, if not, create them
     for dir in directoriesDict:
         if not directoryExist(dir):
             createDirectory(dir)
-    print(len(listdir(downloadPath)))
+    # For each file gather the data on the file and if it should be moved
     for file in listdir(downloadPath):
-        print(checkExtension(file))
+        result = checkExtension(file)
+        if result in directoriesDict:
+            print(result)
+            file = path.join(downloadPath, file)
+            dest = path.join(downloadPath, result)
+            #     print(file,"\n",dest)
+            fileNewPath = copyFiles(file, dest)
+        else:
+            print(f"Was unable to copy {file} because {result}")
+            continue
+
+        # Check each file has successfully been created
+        if validateNewFile(fileNewPath):
+            os.remove(file)
     return 1
 
 
@@ -62,12 +89,25 @@ def checkExtension(file: str) -> str:
     if len(fileBreakdown) <= 1:
         return "No file extension"
     else:
-        name = fileBreakdown[0]
-        extension = fileBreakdown[-1]
+        name = path.splitext(file)[0]
+        print(name)
+        extension = fileBreakdown[-1].lower()
     for category, ext in directoriesDict.items():
         if extension in ext:
             return category
     return f"File {name} with extension {extension} has no defined category!"
+
+
+def copyFiles(file: str, destination: str) -> str:
+    try:
+        result = shutil.copy2(file, destination)
+    except shutil.SameFileError:
+        return f"ERROR: File same as destination"
+    return result
+
+
+def validateNewFile(newPath: str) -> bool:
+    return path.exists(newPath)
 
 
 if __name__ == "__main__":
