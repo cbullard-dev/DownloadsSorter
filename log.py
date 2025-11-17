acb7bd9297c940408ad5daba95c9426c805d38d5
Cal <60569006+cbullard-dev@users.noreply.github.com>
2025-11-17 18:16:36 +0900

    Removed typo error in log.py - removed the " : " that was not a part of the log string - Added spaces between each "" for the .join function

--- log.py
+++ log.py
@@ -1,14 +1,14 @@
 from time import localtime, strftime
 
 def info(*details: str) -> None:
-  print(f"{strftime("%Y%m%d - %H:%M:%S %z",localtime())} : INFO :", "".join(details))
+  print(f"{strftime("%Y%m%d - %H:%M:%S %z",localtime())} : INFO :", " ".join(details))
 
 def debug(*details: str) -> None:
-  print(f"{strftime("%Y%m%d - %H:%M:%S %z",localtime())} : DEBUG :", " : " "".join(details))
+  print(f"{strftime("%Y%m%d - %H:%M:%S %z",localtime())} : DEBUG :", " ".join(details))
 
 def warn(*details: str) -> None:
-  print(f"{strftime("%Y%m%d - %H:%M:%S %z",localtime())} : WARN :", "".join(details))
+  print(f"{strftime("%Y%m%d - %H:%M:%S %z",localtime())} : WARN :", " ".join(details))
 
 def error(*details: str) -> None:
-  print(f"{strftime("%Y%m%d - %H:%M:%S %z",localtime())} : ERROR :", "".join(details))
+  print(f"{strftime("%Y%m%d - %H:%M:%S %z",localtime())} : ERROR :", " ".join(details))
 
