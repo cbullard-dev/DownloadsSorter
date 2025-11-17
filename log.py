26d8844d152c2925aa45ae2ea1e14e2a7df0f819
Cal <60569006+cbullard-dev@users.noreply.github.com>
2025-11-17 17:42:40 +0900

    Combine details in logs - Added combined details using the iterable join

--- log.py
+++ log.py
@@ -1,14 +1,14 @@
 from time import localtime, strftime
 
-def info(details) -> None:
-  print(f"{strftime("%Y%m%d - %H:%M:%S %z",localtime())} : INFO :", details)
+def info(*details: str) -> None:
+  print(f"{strftime("%Y%m%d - %H:%M:%S %z",localtime())} : INFO :", "".join(details))
 
 def debug(*details: str) -> None:
-  print(f"{strftime("%Y%m%d - %H:%M:%S %z",localtime())} : DEBUG :", details)
+  print(f"{strftime("%Y%m%d - %H:%M:%S %z",localtime())} : DEBUG :", " : " "".join(details))
 
 def warn(*details: str) -> None:
-  print(f"{strftime("%Y%m%d - %H:%M:%S %z",localtime())} : WARN :", details)
+  print(f"{strftime("%Y%m%d - %H:%M:%S %z",localtime())} : WARN :", "".join(details))
 
 def error(*details: str) -> None:
-  print(f"{strftime("%Y%m%d - %H:%M:%S %z",localtime())} : ERROR :", details)
+  print(f"{strftime("%Y%m%d - %H:%M:%S %z",localtime())} : ERROR :", "".join(details))
 
